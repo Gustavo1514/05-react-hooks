823d57b5c868d544646fd318932ced65c58a93be
henryGustavo1514 <gustavo_1514@outlook.es>
2020-12-08 17:57:57 -0500

    hooks react

--- src/components/02-useEffect/SimpleForm.js
+++ src/components/02-useEffect/SimpleForm.js
@@ -1,18 +1,54 @@
-import React, { useEffect } from 'react';
-import  './effects.css';
+import React, { useEffect, useState } from 'react'
+import './effects.css'
 
 const SimpleForm = () => {
+  const [formState, setformState] = useState({
+    name: '',
+    email: ''
+  })
 
-    useEffect(()=>{
-console.log('hei');
-    }, )
-    return ( 
-        <>
-        <h1>UseEffect</h1>
-        <hr/>
-        </>
+  const handleInputChange = e => {
+    setformState({ ...formState, [e.target.name]: e.target.value })
+  }
+  const { name, email } = formState
+  useEffect(() => {
+    // console.log('montado')
+  }, [])
 
-     );
+  useEffect(() => {
+    // console.log('formstate change')
+  }, [formState])
+
+  useEffect(() => {
+    // console.log('email change')
+  }, [email])
+  return (
+    <>
+      <h1>UseEffect</h1>
+      <hr />
+
+      <div className='form-group'>
+        <input
+          type='text'
+          name='name'
+          className='form-control'
+          autoComplete='off'
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className='form-group'>
+        <input
+          type='text'
+          name='email'
+          className='form-control'
+          autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
+    </>
+  )
 }
- 
-export default SimpleForm;
+
+export default SimpleForm
