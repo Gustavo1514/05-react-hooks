2be33f17a8a94eeb0ec225f254ba35bab262d60d
henryGustavo1514 <gustavo_1514@outlook.es>
2020-12-21 11:15:12 -0500

    agregando formulario con hook useForm , inicializando reducer todo con init , utilizando localstorage

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,16 +1,50 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { todoReducer } from './todoReducer'
 import './styles.css'
+import { useForm } from '../../hooks/useForm'
 
-const initialState = [
-  {
-    id: new Date().getTime(),
-    description: 'Aprender React',
-    done: false
-  }
-]
+// const initialState = [
+//   {
+//     id: new Date().getTime(),
+//     description: 'Aprender React',
+//     done: false
+//   }
+// ]
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || []
+}
+
 const TodoApp = () => {
-  const [todos] = useReducer(todoReducer, initialState)
+  const [todos, dispatch] = useReducer(todoReducer, [], init)
+  // console.log(todos)
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: ''
+  })
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (description.trim() === '') {
+      return
+    }
+    const newTodo = {
+      id: new Date().getTime(),
+      description: description,
+      done: false
+    }
+
+    const action = {
+      type: 'add',
+      payload: newTodo
+    }
+
+    dispatch(action)
+    reset()
+  }
+
   console.log(todos)
   return (
     <div>
@@ -19,11 +53,11 @@ const TodoApp = () => {
       </h1>
       <hr />
       <div className='row'>
-        <div className='col-7'>
+        <div className='col-sm-7'>
           <ul className='list-group list-group-flush'>
             {todos.map((todo, i) => (
               <li className='list-group-item' key={todo.id}>
-                <p className='text-center'>
+                <p className='text-center '>
                   {i + 1}. {todo.description}
                 </p>
                 <button className='btn btn-danger'>Borrar</button>
@@ -31,7 +65,27 @@ const TodoApp = () => {
             ))}
           </ul>
         </div>
-        <div className='col-5'>Agregar</div>
+        <div className='col-sm-5'>
+          <h4>Agregar TODO</h4>
+          <hr />
+
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              name='description'
+              id=''
+              placeholder='aprender ...'
+              autoComplete='off'
+              className='form-control'
+              value={description}
+              onChange={handleInputChange}
+            />
+
+            <button type='submit' className='btn btn-outline-primary mt-2'>
+              Agregar
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   )
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -3,9 +3,12 @@ import { useState } from 'react'
 export const useForm = (initialState = {}) => {
   const [values, setvalues] = useState(initialState)
 
+  const reset = () => {
+    setvalues(initialState)
+  }
   const handleInputChange = e => {
     setvalues({ ...values, [e.target.name]: e.target.value })
   }
 
-  return [values, handleInputChange] 
+  return [values, handleInputChange, reset]
 }
